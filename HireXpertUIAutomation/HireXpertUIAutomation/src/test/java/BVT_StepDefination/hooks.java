7d9bed407dfa5dfc439fe1201676de7c0388b598
Komal176 <63051509+Komal176@users.noreply.github.com>
2021-01-06 16:44:15 +0530

    ss code

--- HireXpertUIAutomation/HireXpertUIAutomation/src/test/java/BVT_StepDefination/hooks.java
+++ HireXpertUIAutomation/HireXpertUIAutomation/src/test/java/BVT_StepDefination/hooks.java
@@ -1,14 +1,60 @@
 package BVT_StepDefination;
 
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
+
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import utilPackage.baseclass;
+
+import org.apache.commons.io.FileUtils;
+
 
 public class hooks extends baseclass{
 
 //	@jobworkflow,@jobupdate,@interview,@JCWF,@market,@TaskManagement
-	@After("@jobworkflow,@jobupdate,@interview,@JCWF,@market")
-	public void Endtest() throws InterruptedException
+	
+//	Logger log = LoggerHelper.getLogger(hooks.class);
+
+
+	@After()
+	public void screenShot(Scenario scenario) throws InterruptedException, IOException
 	{
+		if (scenario.isFailed()) {
+
+			try {
+//				log.info(scenario.getName() + " is Failed");
+				
+//				final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+//				scenario.embed(screenshot, "image/png"); // ... and embed it in
+				
+				File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			    FileUtils.copyFile(screenshotFile, new File("c:\\hirexprt.png"));
+			
+			} catch (WebDriverException e) {
+				e.printStackTrace();
+			}
+
+		}
+//		else {
+//			try {
+////				log.info(scenario.getName() + " is pass");
+//				scenario.embed(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES), "image/png");
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+	}		
+	
+		@After("@reg1")  
+		public void endTest() throws InterruptedException
+		{
 		if(addjobpage.addJobFlag==1)
 		{
 			workbenchpage.deleteJob();
@@ -38,3 +84,4 @@ public class hooks extends baseclass{
 	driver.quit();	
 	}	
 }
+
