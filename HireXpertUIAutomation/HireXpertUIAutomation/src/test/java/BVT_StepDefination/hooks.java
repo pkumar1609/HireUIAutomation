7cf118f68f4bcec7e49cb4887f854046f97d8c37
Komal176 <63051509+Komal176@users.noreply.github.com>
2021-01-07 20:37:58 +0530

    Update hooks.java

--- HireXpertUIAutomation/HireXpertUIAutomation/src/test/java/BVT_StepDefination/hooks.java
+++ HireXpertUIAutomation/HireXpertUIAutomation/src/test/java/BVT_StepDefination/hooks.java
@@ -37,38 +37,51 @@ public class hooks extends baseclass{
 //		}
 //	}	
 
-		@After("@jobworkflow,@jobupdate,@interview,@JCWF,@market")  
+		@After("@market")  
 		public void endTest() throws InterruptedException
 		{
-		if(addjobpage.addJobFlag==1)
-		{
+//			System.out.println(common.shareFlag+" : common.shareFlag ");
+//			System.out.println(addjobpage.addJobFlag+" : addjobpage.addJobFlag ");
+	
+			loginpage.logoutFromAppK();
+			loginpage.ClickOnEmployerAgencySigninLink();
+			loginpage.login("pemp@gmail.com");
 			workbenchpage.deleteJob();
-			if((loginpage.logedinuser=="pemp" || loginpage.logedinuser=="pa1") && common.shareFlag==1)
-			{
-				loginpage.logoutFromAppK();
-				loginpage.ClickOnEmployerAgencySigninLink();
-				loginpage.loginIn("pagy@gmail.com", "12345");
-				workbenchpage.deleteJob();
-			}
-			else if((loginpage.logedinuser=="pagy" || loginpage.logedinuser=="pe1" ) && common.shareFlag==1)
-			{
-				loginpage.logoutFromAppK();
-				loginpage.ClickOnEmployerAgencySigninLink();
-				loginpage.loginIn("pemp@gmail.com", "12345");
-				workbenchpage.deleteJob();
-			}		
-			if (addjobpage.jobAddedByEmp==false)
-			{
-				loginpage.logoutFromAppK();
-				loginpage.ClickOnEmployerAgencySigninLink();
-				loginpage.loginIn("pemp@gmail.com", "12345");
-				workbenchpage.deleteJob();
-			}	
-		}
-	Thread.sleep(1000);
-	driver.quit();	
-	}	
-		
+			loginpage.logoutFromAppK();
+			loginpage.ClickOnEmployerAgencySigninLink();
+			loginpage.loginIn("pemp@gmail.com", "12345");
+			workbenchpage.deleteJob();		
 			
+//		if(addjobpage.addJobFlag==1)
+//		{
+//			workbenchpage.deleteJob();
+//			if(loginpage.logedinuser=="pemp" || loginpage.logedinuser=="pa1" && common.shareFlag==1)
+//			{
+//				loginpage.logoutFromAppK();
+//				loginpage.ClickOnEmployerAgencySigninLink();
+//				loginpage.loginIn("pagy@gmail.com", "12345");
+//				workbenchpage.deleteJob();
+//			}
+//			else if(loginpage.logedinuser=="pagy" || loginpage.logedinuser=="pe1"  && common.shareFlag==1)
+//			{				
+//				loginpage.logoutFromAppK();
+//				loginpage.ClickOnEmployerAgencySigninLink();
+//				loginpage.loginIn("pemp@gmail.com", "12345");
+//				workbenchpage.deleteJob();
+//			}		
+//			if (addjobpage.jobAddedByEmp==false)
+//			{
+//				loginpage.logoutFromAppK();
+//				loginpage.ClickOnEmployerAgencySigninLink();
+//				loginpage.loginIn("pemp@gmail.com", "12345");
+//				workbenchpage.deleteJob();
+//			}	
+//		}
+	Thread.sleep(1000);
+	driver.quit();
+//	addjobpage.addJobFlag=0;
+//	common.shareFlag=0;
+	}	
+				
 }
 
