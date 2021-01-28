28354033321a97e033d81f2c0a0923cd35ed747f
Komal176 <63051509+Komal176@users.noreply.github.com>
2021-01-28 10:39:27 +0530

    Update baseclass.java

--- HireXpertUIAutomation/HireXpertUIAutomation/src/main/java/utilPackage/baseclass.java
+++ HireXpertUIAutomation/HireXpertUIAutomation/src/main/java/utilPackage/baseclass.java
@@ -125,7 +125,7 @@ public class baseclass extends SuperBase{
 			driver = new ChromeDriver();
 		}	
 		driver.manage().window().setSize(new Dimension(1920, 1080));
-		driver.manage().window().maximize();	
+//		driver.manage().window().maximize();	
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(utilclass.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(utilclass.IMPLICIT_WAIT, TimeUnit.SECONDS);
