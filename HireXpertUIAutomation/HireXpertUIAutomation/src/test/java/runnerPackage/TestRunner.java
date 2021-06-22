1b7a299492f6a9750604ab6ce834e0ea5899d25c
Kalidas <38713543+akashjagtap4471@users.noreply.github.com>
2021-06-22 15:55:15 +0530

    Reverted changes for TestRunner

--- HireXpertUIAutomation/HireXpertUIAutomation/src/test/java/runnerPackage/TestRunner.java
+++ HireXpertUIAutomation/HireXpertUIAutomation/src/test/java/runnerPackage/TestRunner.java
@@ -13,9 +13,9 @@ plugin = {"pretty",
         "html:target/cucumber-reports","json:target/cucumber.json", 
         "junit:C:/Users/admin/AppData/Local/Jenkins.jenkins/workspace/UI Automation/HireXpertUIAutomation/HireXpertUIAutomation/target/cucumber-reports/Cucumber.xml"
         },
-//tags=("@login,@register,@jobworkflow,@jobupdate,@interview,@JobCandidateManagement,@JCWF,@Job,@JobQuestionary")
+tags=("@login,@register,@jobworkflow,@jobupdate,@interview,@JobCandidateManagement,@JCWF,@Job,@JobQuestionary")
 
-tags=("@JobQuestionaryF")
+
 )
 public class TestRunner
 {};
