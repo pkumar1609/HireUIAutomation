25d79c8970261bc4c95f0cacd8f8d50efcc5f66a
Abhishek <38713543+akashjagtap4471@users.noreply.github.com>
2021-07-16 17:07:21 +0530

    Added new test to register new application user after database cleanup.

--- HireXpertUIAutomation/HireXpertUIAutomation/src/main/java/features/UserRegistrationAfterDBCleanUp.feature
+++ HireXpertUIAutomation/HireXpertUIAutomation/src/main/java/features/UserRegistrationAfterDBCleanUp.feature
@@ -4,16 +4,31 @@ Feature: Database CleanUp New User Registration
 Scenario Outline: After database cleanup New Application User registration
 Given User open browser
 When click on Register link
-And New Application User registration to HireXpert "<EmployerName>" "<EmployerNameEmail>" "<ContactNumber>" "Employer" "<Country>" "<TimeZone>" "<Password>" "<Address>" "ehs4ORG" "ehs4ORG.com" "<City>"
+And New HireXpert application user employer registration "<EmployerName>" "<EmployerEmail>" "<ContactNumber>" "Employer" "<Country>" "<TimeZone>" "<Password>" "<Address>" "<EmpOrgName>" "<EmpOrgWebsite>" "<City>"
 And User adds new job
 | JobTitle| Designation|Industry     |FunctionalArea     |Location | Budget | MinSal| MaxSal | MinExp | MaxExp|NoOfInterviews|NoticePeriod| City|CityArea| ZipCode|
 | NewJob |  Developer  | IT software | Software Developer| Pune    | 800000 |350000 | 700000 |   3    |  6    | 2            |60          | Pune |VimanNagar| 42001|
+And Add Manage Employee for loggedIn employer
+          | Name    | Email         | contact | Role          |
+          | pe1     | pe1@gmail.com | 1204564 | Interviewer   |
+          | pe2     | pe2@gmail.com | 1214564 | Recruitment HR|
+When click on Register link
+And New HireXpert application user agency registration "<AgencyName>" "<AgencyEmail>" "<ContactNumber>" "Agency" "<Country>" "<TimeZone>" "<Password>" "<Address>""<AgcyOrgName>" "<AgcyOrgWebsite>" "<City>"
+And Add Manage Employee for loggedIn agency
+          | Nameagy  | Emailagy          | contact | Roleagy                 |
+          | patmbr1  | patmbr1@gmail.com | 1204564 | Recruitment Agency Head |
+          | patmbr2  | patmbr2@gmail.com | 1218564 | Recruitment Agency Head |
+And Add Manage Vendors for loggedIn agency
+          | Name    | Email            | contact |
+          | pagvdr1 | pagvdr1@gmail.com| 1204564 |
+          | pagvdr2 | pagvdr2@gmail.com| 1218564 |
+Given User logged in to HireXpert "<EmployerEmail>" and "<Password>" 
 And Add Manage Recruiters for loggedIn employee
-          | Name   | Email           | contact | Role          |
-          | tsmbr1 | tsmbr1@gmail.com| 1234564 | Recruitment HR|
-#And Logout from App
+          | Name    | Email         | contact  | Role          |
+          | pagy    | pagy@gmail.com| 12304564 | Recruitment HR|
+And Logout from App
 Examples:
-|EmployerName|EmployerNameEmail|AgencyName|AgencyEmail     |Password|ContactNumber|Country |City  |Address     | ZipCode|TimeZone                    				   |
-|ehs4        |ehs4@gmail.com |  AAgcy10  |AAgcy10@gmail.com|12345   | 6820601966  | India  | Pune | Viman Nagar| 411014 |(GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi|
+|EmployerName|EmployerEmail |EmpOrgName|EmpOrgWebsite|AgencyName|AgencyEmail   |AgcyOrgName|AgcyOrgWebsite|Password|ContactNumber|Country|City  |Address     | ZipCode|TimeZone                    				      |
+|pemp        |pemp@gmail.com|pempORG   |pempORG.com  | pagy     |pagy@gmail.com|pagyORG    |pagyORG.com   |12345   |6820601966   | India | Pune | Viman Nagar| 411014 |(GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi|
 
 
--- HireXpertUIAutomation/HireXpertUIAutomation/src/test/java/BVT_StepDefination/UserRegistrationAfterDBCleanUpStepDefination.java
+++ HireXpertUIAutomation/HireXpertUIAutomation/src/test/java/BVT_StepDefination/UserRegistrationAfterDBCleanUpStepDefination.java
@@ -8,72 +8,130 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import utilPackage.baseclass;
 
-public class UserRegistrationAfterDBCleanUpStepDefination extends baseclass{
+public class UserRegistrationAfterDBCleanUpStepDefination extends baseclass {
 
 	Boolean userAlreadyRegistered = false;
 
-	@When("^Check LoggedIn User Usertype$")
-	public void check_LoggedIn_User_Usertype() throws Throwable {
-	  
-		loginpage.identifyUserK();
-	}
-	
-	@When("^New Application User registration to HireXpert \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$")
-	public void new_Application_User_registration_to_HireXpert(String LoggedInUserName, String LoginEmail, String ContactNumber, String UserType, String Country, String TimeZone, String Password, String Address, String OrganizationName, String Website, String City) throws Throwable {
-	
-	    try
-	    {
-	    	String errorMessageForAlreadyRegisteredUser = "";	
-			registerpage.registeremployerdetails(LoggedInUserName, LoginEmail,ContactNumber);		
-			registerpage.registerUserdetails(UserType,TimeZone,Country);		
+	@When("^New HireXpert application user employer registration \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$")
+	public void new_HireXpert_application_user_employer_registration(String EmployerName, String EmployerEmail,
+			String ContactNumber, String UserType, String Country, String TimeZone, String Password, String Address,
+			String EmpOrgName, String EmpOrgWebsite, String City) throws Throwable {
+
+		try {
+			String errorMessageForAlreadyRegisteredUser = "";
+			registerpage.registeremployerdetails(EmployerName, EmployerEmail, ContactNumber);
+			registerpage.registerUserdetails(UserType, TimeZone, Country);
 			common.ClickSumbit();
 			common.clickOnConfirmYes();
-					
-		    errorMessageForAlreadyRegisteredUser = driver.findElement(By.cssSelector("div.col-md-12.error")).getText();
-		     
-		 	if(errorMessageForAlreadyRegisteredUser.length() > 0 && errorMessageForAlreadyRegisteredUser.contains("already register with"))
-			{
-		 		userAlreadyRegistered = true;
-		 		
+
+			errorMessageForAlreadyRegisteredUser = driver.findElement(By.cssSelector("div.col-md-12.error")).getText();
+
+			if (errorMessageForAlreadyRegisteredUser.length() > 0
+					&& errorMessageForAlreadyRegisteredUser.contains("already register with")) {
+				userAlreadyRegistered = true;
+
 				common.clickOnCloseBtn();
 				common.clickOnConfirmYes();
-			}		     
-	    }
-	    catch(NoSuchElementException ex)
-	    {
-	    	userAlreadyRegistered = false;
-	    	common.clickOnOKBtn();	    	
-	    	loginpage.ClickOnEmployerAgencySigninLink();
-			loginpage.loginInNew(LoginEmail, "12345");
-			updateprofilepopuppage.UpdateProfile(Address, OrganizationName, Website, City);
+			}
+		} catch (NoSuchElementException ex) {
+			userAlreadyRegistered = false;
+			common.clickOnOKBtn();
+			loginpage.ClickOnEmployerAgencySigninLink();
+			loginpage.loginInNew(EmployerEmail, Password);
+			updateprofilepopuppage.UpdateProfile(Address, EmpOrgName, EmpOrgWebsite, City);
 			common.ClickSumbit();
 			loginpage.identifyUserK();
-			//add new job
-	    }	 
+		}
 	}
 
 	@When("^User adds new job$")
 	public void user_adds_new_job(DataTable credentials) throws Throwable {
-	  
-		if(!userAlreadyRegistered)
-		{
+
+		if (!userAlreadyRegistered) {
 			dashboardpage.openDashboardPage();
-			dashboardpage.AddJob();				
-			addjobpage.filljobDetails(credentials);		
+			dashboardpage.AddJob();
+			addjobpage.filljobDetails(credentials);
 			common.ClickSumbit();
 			common.clickOnOKBtn();
 			dashboardpage.openWorkbenchPage();
-		}	
+		}
 	}
 
 	@Given("^Add Manage Recruiters for loggedIn employee$")
 	public void add_Manage_Recruiters_for_loggedIn_employee(DataTable credentials) throws Throwable {
-	 		
-		if(!userAlreadyRegistered)
-		{
-			managerecruitmentagencies.addRecruiters(credentials);			
-		}		
+
+		if (!userAlreadyRegistered) {
+			managerecruitmentagencies.addRecruiters(credentials);
+		}
 	}
 
+	@When("^Add Manage Employee for loggedIn employer$")
+	public void add_Manage_Employee_for_loggedIn_employer(DataTable credentials) throws Throwable {
+
+		if (!userAlreadyRegistered) {
+			manageemployee.addEmployee(credentials);
+			// logout
+			loginpage.logoutFromAppK();
+			common.logout.click();
+			if (common.okbtnPopup.size() > 0) {
+				common.clickOnOKBtn();
+			}
+		}
+	}
+
+	// Agency
+	@When("^New HireXpert application user agency registration \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"\"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$")
+	public void new_HireXpert_application_user_agency_registration(String AgencyName, String AgencyEmail,
+			String ContactNumber, String UserType, String Country, String TimeZone, String Password, String Address,
+			String AgcyOrgName, String AgcyOrgWebsite, String City) throws Throwable {
+
+		try {
+			String errorMessageForAlreadyRegisteredUser = "";
+			registerpage.registeremployerdetails(AgencyName, AgencyEmail, ContactNumber);
+			registerpage.registerUserdetails(UserType, TimeZone, Country);
+			common.ClickSumbit();
+			common.clickOnConfirmYes();
+
+			errorMessageForAlreadyRegisteredUser = driver.findElement(By.cssSelector("div.col-md-12.error")).getText();
+
+			if (errorMessageForAlreadyRegisteredUser.length() > 0
+					&& errorMessageForAlreadyRegisteredUser.contains("already register with")) {
+
+				userAlreadyRegistered = true;
+				common.clickOnCloseBtn();
+				common.clickOnConfirmYes();
+			}
+		} catch (NoSuchElementException ex) {
+			userAlreadyRegistered = false;
+			common.clickOnOKBtn();
+			loginpage.ClickOnEmployerAgencySigninLink();
+			loginpage.loginInNew(AgencyEmail, Password);
+			updateprofilepopuppage.UpdateProfile(Address, AgcyOrgName, AgcyOrgWebsite, City);
+			common.ClickSumbit();
+			loginpage.identifyUserK();
+		}
+	}
+
+	@When("^Add Manage Employee for loggedIn agency$")
+	public void add_Manage_Employee_for_loggedIn_agency(DataTable credentials) throws Throwable {
+
+		if (!userAlreadyRegistered) {
+			manageemployee.addEmployee(credentials);
+		}
+	}
+
+	@When("^Add Manage Vendors for loggedIn agency$")
+	public void add_Manage_Vendors_for_loggedIn_agency(DataTable credentials) throws Throwable {
+
+		if (!userAlreadyRegistered) {
+			managevendor.addVendor(credentials);
+			// logout
+			loginpage.logoutFromAppK();		
+			common.logout.click();
+			if (common.okbtnPopup.size() > 0) {
+				common.clickOnOKBtn();
+			}
+		}
+	}
 
 }
