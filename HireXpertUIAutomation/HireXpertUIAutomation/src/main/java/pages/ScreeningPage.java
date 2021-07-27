e8473c14fa0afb0d69fdf18d5805426206da321d
Abhishek <38713543+akashjagtap4471@users.noreply.github.com>
2021-07-27 19:15:58 +0530

    Added new controls for screening page.

--- HireXpertUIAutomation/HireXpertUIAutomation/src/main/java/pages/ScreeningPage.java
+++ HireXpertUIAutomation/HireXpertUIAutomation/src/main/java/pages/ScreeningPage.java
@@ -35,12 +35,45 @@ public class ScreeningPage extends baseclass {
 
 	@FindBy(xpath = "//select[@placeholder='Reject Percentage']")
 	private WebElement rejectPercent;
+	
+	@FindBy(xpath = "//button[@title='edit' and contains(text(),'Edit')]")
+	private WebElement editButton;
+	
+	@FindBy(xpath = "//button[contains(text(),'Publish')]")
+	private WebElement publishButton;
+		
+	@FindBy(xpath = "//button[contains(text(),'Disable')]")
+	private WebElement disableButton;
 
 	@FindBy(xpath = "//button[contains(text(),'Add New Question')]")
 	private WebElement addNewQuestionButton;
 
 	@FindBy(xpath = "//h5[contains(text(),'Add Question')]")
 	private WebElement addNewQuestionPopUp;
+
+	@FindBy(xpath = "//select[@formcontrolname='QuestionType']")
+	private WebElement questionType;
+
+	@FindBy(xpath = "(//input[@formcontrolname='Marks'])[1]")
+	private WebElement mainMarks;
+
+	@FindBy(xpath = "//input[@placeholder='Question Time In Minutes']")
+	private WebElement questionTime;
+
+	@FindBy(xpath = "//select[@formcontrolname='NeedExplanationofAnswer']")
+	private WebElement explanationRequired;
+
+	@FindBy(xpath = "//textarea[@id='Question']")
+	private WebElement questionTextArea;
+
+	@FindBy(xpath = "//input[@placeholder='Answer']")
+	private WebElement answer;
+
+	@FindBy(xpath = "//input[@placeholder='Mark']")
+	private WebElement mark;
+
+	@FindBy(xpath = "//button[contains(text(),'Save Changes')]")
+	private WebElement saveChangesButton;
 
 	public ScreeningPage() {
 		PageFactory.initElements(driver, this);
@@ -99,4 +132,55 @@ public class ScreeningPage extends baseclass {
 		return false;
 	}
 
+	public WebElement getQuestionType() {
+		return questionType;
+	}
+
+	public WebElement getScreeningMainMarks() {
+		return mainMarks;
+	}
+
+	public WebElement getScreeningQuestionTime() {
+		return questionTime;
+	}
+
+	public WebElement getScreeningExplanationRequired() {
+		return explanationRequired;
+	}
+
+	public WebElement getScreeningQuestionTextArea() {
+		return questionTextArea;
+	}
+
+	public WebElement getScreeningGridAnswer() {
+		return answer;
+	}
+
+	public WebElement getScreeningGridMark() {
+		return mark;
+	}
+
+	public WebElement getScreeningSaveChangesButton() {
+		return saveChangesButton;
+	}
+
+	public WebElement getScreeningPublishButton() {
+		return publishButton;
+	}	
+
+	public WebElement getScreeningEditButton() {
+		return editButton;
+	}	
+	
+	public WebElement getScreeningDisableButton() {
+		return disableButton;
+	}
+	
+	public boolean getAddEditGridQuestionRrow(String gridQuestion) {
+		WebElement addEditGridQuestionRrow = driver.findElement(By.xpath("//textarea[contains(text(),'"+gridQuestion+"')]"));
+		if (addEditGridQuestionRrow.isDisplayed())
+			return true;
+		return false;
+	}
+	
 }
